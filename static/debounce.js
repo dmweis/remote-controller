f665680aaa9752700cbd4eb7079ad51d52a10d70
David Weis <dweis7@gmail.com>
2021-02-22 00:02:59 +0000

    Remove logging from debouncer

--- static/debounce.js
+++ static/debounce.js
@@ -13,11 +13,9 @@ class Debouncer {
     */
     interval_event() {
         if (this.last_written) {
-            console.log("sending from interval");
             this.connection.send(JSON.stringify(this.last_written));
             this.last_written = null;
         } else {
-            console.log("clearing interval");
             clearInterval(this.interval_id);
             this.interval_id = null;
         }
@@ -28,14 +26,12 @@ class Debouncer {
      */
     send(message) {
         if (this.interval_id == null) {
-            console.log("sending first");
             this.connection.send(JSON.stringify(message));
             const captured = this;
             this.interval_id = setInterval(function () {
                 captured.interval_event();
             }, 100);
         } else {
-            console.log("updating active interval");
             this.last_written = message;
         }
     }
