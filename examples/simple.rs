225e664031e32b1c6aac91ff54849783070766bd
David Weis <dweis7@gmail.com>
2021-03-08 00:42:24 +0000

    Remove useless mut

--- examples/simple.rs
+++ examples/simple.rs
@@ -6,7 +6,7 @@ async fn main() {
     std::env::set_var("RUST_LOG", "info");
     env_logger::init();
 
-    let mut state_handle = remote_controller::start_remote_controller_server(([0, 0, 0, 0], 8080));
+    let state_handle = remote_controller::start_remote_controller_server(([0, 0, 0, 0], 8080));
 
     loop {
         sleep(Duration::from_millis(20)).await;
